8f13840d507e9fa4dc85d7ea9a989982bec14936
Edjery <edisgumbao@gmail.com>
2024-11-18 17:24:18 +0800

    set port to local

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=psgc',
+    'dsn' => 'mysql:host=localhost:3307;dbname=psgc',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8mb4',
